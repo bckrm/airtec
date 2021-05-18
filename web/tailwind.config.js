67b32cea87a4a041670203c3f22a1fc03a614332
kcbreuil <kcbreuil@gmail.com>
2021-05-18 14:21:41 -0400

    add font sizes to config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
     purge: ['./src/**/*.js', './src/**/*.jsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontSize: {
+                'ts-h1': ['3.052rem'],
+                'ts-h2': ['2.441rem'],
+                'ts-h3': ['1.953rem'],
+                'ts-h4': ['1.563rem'],
+                'ts-h5': ['1.25rem'],
+                'ts-lg-body': ['1.25rem'],
+                'ts-body': ['1rem'],
+                'ts-caption': ['0.8rem'],
+                'ts-attribution': ['0.512rem'],
+            },
+        },
     },
     variants: {
         extend: {},
