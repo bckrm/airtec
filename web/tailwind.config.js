6e10393419d70a4c9aced151e19a7b5914746166
Katie Breuil <61708936+kcbreuil@users.noreply.github.com>
2021-05-18 13:56:00 -0400

    Add color palette to tw config (#2)
    * remove package-lock
    
    * replace color names with brand var

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
     purge: ['./src/**/*.js', './src/**/*.jsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'brand-1': '#12163B',
+                // darkest-blue
+                'brand-2': '#282768',
+                // dark-blue
+                'brand-3': '#E81E25',
+                // red
+                'brand-4': '#6BABE5',
+                // light-blue
+                'brand-5': '#ACC9E8',
+                // lightest-blue
+                'brand-6': '#3F2B56',
+                // purple
+                'brand-7': '#D4C0CD',
+                // light-purple
+            },
+        },
     },
     variants: {
         extend: {},
