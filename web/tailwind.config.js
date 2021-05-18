9b402da31521738c5c0582cc3993055d77f8afc6
kcbreuil <kcbreuil@gmail.com>
2021-05-18 13:17:31 -0400

    remove package-lock

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
     purge: ['./src/**/*.js', './src/**/*.jsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'brand-darkest-blue': '#12163B',
+                'brand-dark-blue': '#282768',
+                'brand-red': '#E81E25',
+                'brand-light-blue': '#6BABE5',
+                'brand-lightest-blue': '#ACC9E8',
+                'brand-purple': '#3F2B56',
+                'brand-light-purple': '#D4C0CD',
+            },
+        },
     },
     variants: {
         extend: {},
