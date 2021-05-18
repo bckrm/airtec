5c1ceeef11b992c1dd7623db0231793ca0bf6d6e
Katie Breuil <61708936+kcbreuil@users.noreply.github.com>
2021-05-18 14:29:23 -0400

    add font sizes to config (#3)

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -19,6 +19,17 @@ module.exports = {
                 'brand-7': '#D4C0CD',
                 // light-purple
             },
+            fontSize: {
+                'ts-h1': ['3.052rem'],
+                'ts-h2': ['2.441rem'],
+                'ts-h3': ['1.953rem'],
+                'ts-h4': ['1.563rem'],
+                'ts-h5': ['1.25rem'],
+                'ts-lg-body': ['1.25rem'],
+                'ts-body': ['1rem'],
+                'ts-caption': ['0.8rem'],
+                'ts-attribution': ['0.512rem'],
+            },
         },
     },
     variants: {
