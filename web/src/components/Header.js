015eadde270fe0c9e9d4c1d76f6e5f79b5bf53a8
kcbreuil <kcbreuil@gmail.com>
2021-05-19 11:22:39 -0400

    first commit

---
+++ web/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const nav = [
+    {
+        name: 'about',
+        link: '/#',
+    },
+    {
+        name: 'capabilities',
+        link: '/#',
+    },
+    {
+        name: 'news',
+        link: '/#',
+    },
+    {
+        name: 'careers',
+        link: '/#',
+    },
+    {
+        name: 'contact',
+        link: '/#',
+    },
+];
+
+export default function Header() {
+    return <nav>Header</nav>;
+}
---
+++ web/src/components/navigation/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+export default function MainNav({ navItems }) {
+    return (
+        <ul className="hidden lg:flex flex-row">
+            {navItems.map((item) => {
+                return (
+                    <li key={item.link}>
+                        <Link to={`${item.link}`}>{item.name}</Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+
+MainNav.propTypes = {
+    navItems: PropTypes.array.isRequired,
+};
