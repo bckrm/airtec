f94babe3dcd7c0e5f0fca8e95949aae649933240
kcbreuil <kcbreuil@gmail.com>
2021-05-21 14:29:18 -0400

    footer ready for review

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -44,24 +44,25 @@ export default function Footer() {
         },
     ];
     return (
-        <footer className="container m-auto text-white">
-            <div className="flex justify-between">
-                <div className="flex space-x-16">
+        <footer className="bg-brand-1 text-ts-body w-full h-96 text-white font-normal fixed bottom-0">
+            <div className="flex justify-between container pt-11">
+                <div className="flex pl-20 space-x-16">
                     <AIRtecAwhite />
-                    <div className="grid grid-cols-2 mr-16 gap-5">
+                    <div className="grid grid-cols-2 gap-x-24 gap-y-5 mt-16">
                         {internalLinks.map((link) => {
                             return <Link to={link.link}>{link.text}</Link>;
                         })}
                     </div>
                 </div>
-                <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-5 mt-16">
                     {externalLinks.map((link) => {
                         return <a href={link.link}>{link.text}</a>;
                     })}
                 </div>
             </div>
-
-            <p className="text-center">Mission Driven. Future Focused.</p>
+            <div className=" mt-20 text-center ">
+                <p>Mission Driven. Future Focused.</p>
+            </div>
         </footer>
     );
 }
