1d38ab269738c791658c36d4015658fd930b2869
kcbreuil <kcbreuil@gmail.com>
2021-05-21 11:14:15 -0400

    wip

---
+++ web/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function Footer() {
+    return (
+        <footer className="text-white">
+            <div>
+                <Link to="/" />
+            </div>
+        </footer>
+    );
+}
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default function IndexPage() {
     return (
         <>
             <Header />
             <h1>index page</h1>
+            <Footer />
         </>
     );
 }
