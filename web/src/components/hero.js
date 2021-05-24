6a048cd4453025be8d98f4c9c3f1b8da0c963660
kcbreuil <kcbreuil@gmail.com>
2021-05-24 17:24:08 -0400

    fixbg image

--- web/src/components/hero.js
+++ web/src/components/hero.js
@@ -8,8 +8,8 @@ export default function Hero({ image }) {
     } = image;
 
     return (
-        <BackgroundImage fluid={fluid}>
-            <h1 className="absolute left-2/4 top-2/4 transform -translate-y-2/4 -translate-x-2/4 text-ts-h1">
+        <BackgroundImage className="w-full min-h-screen" fluid={fluid}>
+            <h1 className="absolute left-2/4 top-2/4 transform -translate-y-2/4 -translate-x-2/4 text-ts-h1 text-white">
                 Donec eleifend faucibus finibus maximus
             </h1>
         </BackgroundImage>
