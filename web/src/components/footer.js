73a9317f4d6e3da1aae1102b3e7f95335adbdb00
Katie Breuil <61708936+kcbreuil@users.noreply.github.com>
2021-05-24 08:46:28 -0400

    Add footer (#9)
    * wip
    
    * updates to footer links
    
    * layout for footer
    
    * footer ready for review
    
    * move footer to layout
    
    * add font file
    
    * remove commented code
    
    * add link for logo

---
+++ web/src/components/footer.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import AIRtecAwhite from './svgs/AIRtecAwhite';
+
+export default function Footer() {
+    const externalLinks = [
+        {
+            link: '/',
+            text: 'Link',
+        },
+        {
+            link: '/',
+            text: 'Link',
+        },
+        {
+            link: '/',
+            text: 'Link',
+        },
+    ];
+    const internalLinks = [
+        {
+            link: '/',
+            text: 'Link',
+        },
+        {
+            link: '/',
+            text: 'Link',
+        },
+        {
+            link: '/',
+            text: 'Link',
+        },
+        {
+            link: '/',
+            text: 'Link',
+        },
+        {
+            link: '/',
+            text: 'Link',
+        },
+        {
+            link: '/',
+            text: 'Link',
+        },
+    ];
+    return (
+        <footer className="bg-brand-1 bottom-0 fixed h-96 text-white w-full">
+            <div className="container flex justify-between pt-11">
+                <div className="flex pl-20 space-x-16">
+                    <Link to="/">
+                        <AIRtecAwhite />
+                    </Link>
+
+                    <div className="grid grid-cols-2 gap-x-24 gap-y-5 mt-16">
+                        {internalLinks.map((link) => {
+                            return <Link to={link.link}>{link.text}</Link>;
+                        })}
+                    </div>
+                </div>
+                <div className="flex flex-col gap-5 mt-16">
+                    {externalLinks.map((link) => {
+                        return <a href={link.link}>{link.text}</a>;
+                    })}
+                </div>
+            </div>
+            <div className="mt-20 text-center">
+                <p>Mission Driven. Future Focused.</p>
+            </div>
+        </footer>
+    );
+}
--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './header';
+import Footer from './footer';
 
 export default function Layout({ children }) {
     return (
         <>
             <Header />
             <main>{children}</main>
+            <Footer />
         </>
     );
 }
