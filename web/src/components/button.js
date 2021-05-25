7ba5072d1468e7b267989b5e42db3482985861fb
kcbreuil <kcbreuil@gmail.com>
2021-05-25 11:26:57 -0400

    button

---
+++ web/src/components/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Button() {
+    return (
+        <button
+            type="button"
+            className="bg-black border-2 h-9 rounded-bl-lg text-white uppercase w-48"
+        >
+            Learn More
+        </button>
+    );
+}
