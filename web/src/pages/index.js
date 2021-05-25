04105082ac7e1663b2539e818016efa038a8f44c
Katie Breuil <61708936+kcbreuil@users.noreply.github.com>
2021-05-25 11:19:43 -0400

    Add hero component (#10)
    * add hero component
    
    * update to gatsby img
    
    * check
    
    * check
    
    * fixbg image
    
    * remove fixed on footer
    
    * add fixed and z index on header
    
    * change styling to flex
    
    * fix Tag

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 
-export default function IndexPage() {
+export default function IndexPage({ data }) {
+    const { heroImage } = data;
     return (
         <Layout>
-            <h1>index page</h1>
+            <Hero image={heroImage} />
         </Layout>
     );
 }
+
+export const query = graphql`
+    query IndexQuery {
+        heroImage: file(relativePath: { regex: "/hero/" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`;
+
+IndexPage.propTypes = {
+    data: PropTypes.shape({
+        heroImage: PropTypes.object.isRequired,
+    }).isRequired,
+};
