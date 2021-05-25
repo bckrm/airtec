fe2bdaf50279d4053168a014b7fc3b701779d40c
kcbreuil <kcbreuil@gmail.com>
2021-05-25 17:36:36 -0400

    remove mb

--- web/src/components/newsList.js
+++ web/src/components/newsList.js
@@ -5,7 +5,7 @@ import NewsCard from './newsCard';
 
 export default function NewsList({ news }) {
     return (
-        <div className="bg-brand-1 h-screen mb-24">
+        <div className="bg-brand-1 h-screen">
             <h1 className="pt-12 text-center text-white text-4xl uppercase">
                 News
                 <span className="absolute bg-brand-4 h-1 left-[48%] text-brand-4 top-[119%] w-10" />
