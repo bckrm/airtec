35791f6408ee44678673c0b6368a5e171f16c03b
kcbreuil <kcbreuil@gmail.com>
2021-05-26 16:27:15 -0400

    button styles

--- web/src/components/button.js
+++ web/src/components/button.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 export default function Button() {
+    const primaryStyle =
+        'bg-black text-white hover:bg-brand-1 visited:bg-gray-300';
+    // const primaryDarkStyle =
+    //     'bg-white text-black hover:bg-gray-300 visited:bg-brand-1';
+    // const secondaryStyle =
+    //     'bg-white text-black hover:bg-brand-1 hover:text-brand-1 visited:text-gray-300';
+    // const secondaryDarkStyle =
+    //     'bg-brand-1 text-white hover:text-gray-300 visited:text-brand-1';
+
     return (
         <button
             type="button"
-            className="bg-black border-2 h-9 rounded-bl-lg text-white uppercase w-48"
+            className={`border-2 border-current h-9 rounded-bl-lg uppercase w-48 ${primaryStyle}`}
         >
             Learn More
         </button>
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Button from '../components/button';
 
 export default function IndexPage() {
     return (
         <Layout>
             <h1>index page</h1>
+            <Button />
         </Layout>
     );
 }
