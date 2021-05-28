f40c0091496ce4843d3cc7816d1c66dbc381d77b
kcbreuil <kcbreuil@gmail.com>
2021-05-28 15:22:53 -0400

    fix width of card

--- web/src/components/card.js
+++ web/src/components/card.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Card() {
     return (
-        <div className="absolute bg-white ml-10 rounded-bl-lg w-5/12">
+        <div className="absolute bg-white mx-4 md:ml-10 rounded-bl-lg w-11/12 md:w-5/12">
             {/* left-24 rounded-bl-lg top-44 w-5/12 */}
             <h1 className="header-accent pl-6 pt-7 mb-6 text-brand-1 text-4xl uppercase">
                 Hello
             </h1>
-            <p className="max-w-md mt-6 pl-6 pb-7">
+            <p className="mt-6 pl-6 pb-7">
                 Donec et sapien turpis. In rutrum nibh id purus venenatis, at
                 vehicula massa faucibus. Maecenas interdum nulla lorem, nec
                 vestibulum nibh tincidunt feugiat. Aenean vulputate eu justo eu
