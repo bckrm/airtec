bc88af7e1ce9f9ba9541550d4d8e581539cbc8ee
kcbreuil <kcbreuil@gmail.com>
2021-05-28 14:07:47 -0400

    add conditional

--- web/src/components/navigation/mainNav.js
+++ web/src/components/navigation/mainNav.js
@@ -5,7 +5,7 @@ import { Link } from 'gatsby';
 export default function MainNav({ navItems }) {
     return (
         <ul className="flex font-semibold gap-3 justify-end mt-16 mr-24 text-white uppercase">
-            {navItems.map((item) => {
+            {navItems.map((item, i) => {
                 return (
                     <li key={item.link} className="group">
                         <Link
@@ -15,18 +15,20 @@ export default function MainNav({ navItems }) {
                             {item.name}
                         </Link>
                         <ul>
-                            {navItems[0].subnav.map((subitem) => {
-                                return (
-                                    <li key={subitem.sublink}>
-                                        <Link
-                                            className={`hover-list-link group-hover:block text-brand-1 bg-${subitem.sublinkcolor}`}
-                                            to={`${subitem.subnav}`}
-                                        >
-                                            {subitem.sublink}
-                                        </Link>
-                                    </li>
-                                );
-                            })}
+                            {navItems[i].subnav
+                                ? navItems[0].subnav.map((subitem) => {
+                                      return (
+                                          <li key={subitem.sublink}>
+                                              <Link
+                                                  className={`hover-list-link group-hover:block text-brand-1 bg-${subitem.sublinkcolor}`}
+                                                  to={`${subitem.subnav}`}
+                                              >
+                                                  {subitem.sublink}
+                                              </Link>
+                                          </li>
+                                      );
+                                  })
+                                : ' '}
                         </ul>
                     </li>
                 );
