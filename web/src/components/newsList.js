83073ff342337f43988869ac32bdab5f854c88f7
kcbreuil <kcbreuil@gmail.com>
2021-06-01 16:44:54 -0400

    get carousel working

--- web/src/components/newsList.js
+++ web/src/components/newsList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 import NewsCard from './newsCard';
 
@@ -12,15 +14,24 @@ export default function NewsList({ news }) {
                 News
                 <span className="absolute bg-brand-4 h-1 left-[48%] text-brand-4 top-[119%] w-10" />
             </h1>
-            <div className="container flex gap-9 m-auto mt-32">
-                {news.map((newsItem) => {
-                    return <NewsCard data={newsItem} />;
-                })}
+            <div className=" flex gap-9 m-auto mt-32">
+                <Carousel
+                    plugins={[
+                        'infinite',
+                        'arrows',
+                        {
+                            resolve: slidesToShowPlugin,
+                            options: {
+                                numberOfSlides: 3.5,
+                            },
+                        },
+                    ]}
+                >
+                    {news.map((newsItem) => {
+                        return <NewsCard data={newsItem} />;
+                    })}
+                </Carousel>
             </div>
-            {/* this button will be changed out */}
-            <button className="text-white" type="button">
-                More
-            </button>
         </div>
     );
 }
