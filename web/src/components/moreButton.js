c3f3556cf9661f0f9ca9540be319948e39dca99f
kcbreuil <kcbreuil@gmail.com>
2021-06-07 12:46:04 -0400

    resposition buttons

--- web/src/components/moreButton.js
+++ web/src/components/moreButton.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 export default function MoreButton({ text, isDisabled, isLeft }) {
     const disabledStyle = 'text-brand-2';
-    const rightStyle = 'bottom-[-10%] right-8';
-    const leftStyle = 'top-[104%] right-[147px]';
+    const rightStyle = 'bottom-[-10.2%] right-8';
+    const leftStyle = 'top-[105%] right-[147px]';
 
     const finalStyles = `absolute border-2 border-current rounded-bl-lg text-white text-center w-24 focus:text-brand-9 ${
         isDisabled ? disabledStyle : 'bg-transparent'
