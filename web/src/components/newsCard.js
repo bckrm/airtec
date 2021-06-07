1e165cea0dec4bbbe09d3a8b7726fbf83e0abcd7
kcbreuil <kcbreuil@gmail.com>
2021-06-07 10:59:38 -0400

    add Link

--- web/src/components/newsCard.js
+++ web/src/components/newsCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
+import { Link } from 'gatsby';
 
 export default function NewsCard({ data }) {
     const {
@@ -12,13 +13,13 @@ export default function NewsCard({ data }) {
     } = data;
 
     return (
-        <div className="bg-white h-[34rem] w-80">
+        <Link to="/" className="bg-white h-[34rem] w-80">
             <Img className="rounded-bl-lg" fluid={fluid} />
             <h3 className="heading-accent font-bold mt-4 pb-10 px-4 text-brand-1 text-ts-h4">
                 {title}
             </h3>
             <p className="px-4">{description}</p>
-        </div>
+        </Link>
     );
 }
 
