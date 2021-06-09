8e7b43ea18be34c8fc0cd2001e4e522b76619de9
kcbreuil <kcbreuil@gmail.com>
2021-06-09 15:10:13 -0400

    apply hover styles to news card

--- web/src/components/newsCard.js
+++ web/src/components/newsCard.js
@@ -13,7 +13,10 @@ export default function NewsCard({ data }) {
     } = data;
 
     return (
-        <Link to="/" className="bg-white h-[34rem] w-80">
+        <Link
+            to="/"
+            className="bg-white h-[34rem] w-80 transform focus:scale-110 md:hover:scale-110"
+        >
             <Img className="rounded-bl-4xl" fluid={fluid} />
             <h3 className="heading-accent font-bold mt-4 pb-10 px-4 text-brand-1 text-ts-h4">
                 {title}
