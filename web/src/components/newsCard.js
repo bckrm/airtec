beeb5583efb745a3819adc00ddb353afc4ccaf99
kcbreuil <kcbreuil@gmail.com>
2021-06-09 14:48:21 -0400

    update border radius of image on news card

--- web/src/components/newsCard.js
+++ web/src/components/newsCard.js
@@ -14,7 +14,7 @@ export default function NewsCard({ data }) {
 
     return (
         <Link to="/" className="bg-white h-[34rem] w-80">
-            <Img className="rounded-bl-lg" fluid={fluid} />
+            <Img className="rounded-bl-4xl" fluid={fluid} />
             <h3 className="heading-accent font-bold mt-4 pb-10 px-4 text-brand-1 text-ts-h4">
                 {title}
             </h3>
