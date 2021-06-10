581c2b1da049323f40503b1a72d3626401a14960
kcbreuil <kcbreuil@gmail.com>
2021-06-09 22:18:03 -0400

    update space between cards

--- web/src/components/newsCard.js
+++ web/src/components/newsCard.js
@@ -15,7 +15,7 @@ export default function NewsCard({ data }) {
     return (
         <Link
             to="/"
-            className="bg-white h-[34rem] mr-4 transform focus:scale-110 md:hover:scale-110 w-80"
+            className="bg-white h-[34rem] mr-9 transform focus:scale-110 md:hover:scale-110 w-80"
         >
             <Img className="rounded-bl-4xl" fluid={fluid} />
             <h3 className="heading-accent font-bold mt-4 pb-10 px-4 text-brand-1 text-ts-h4">
