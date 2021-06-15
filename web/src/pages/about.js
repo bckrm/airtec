bf9b6eec058f276b75854dee6ae2a2a4919b6fac
Katie Breuil <kcbreuil@gmail.com>
2021-06-15 12:54:12 -0400

    remove component

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -6,7 +6,6 @@ import Layout from '../components/layout';
 import Hero from '../components/hero';
 import LeaderSection from '../components/leaderSection';
 import AboutText from '../components/aboutTextSection';
-import CertificationSection from '../components/certificationSection';
 
 export default function AboutPage({ data }) {
     const { aboutImage } = data;
@@ -58,25 +57,11 @@ export default function AboutPage({ data }) {
         },
     ];
 
-    const certifications = [
-        {
-            title: 'Nulla fermentum massa',
-            description:
-                'Proin condimentum dolor elementum augue fermentum pretium. Phasellus consequat',
-        },
-        {
-            title: 'Nulla fermentum massa',
-            description:
-                'Proin condimentum dolor elementum augue fermentum pretium. Phasellus consequat',
-        },
-    ];
-
     return (
         <Layout>
             <Hero data={content.hero} />
             <AboutText data={content.about} />
             <LeaderSection leaders={leaders} />
-            <CertificationSection certifications={certifications} />
         </Layout>
     );
 }
