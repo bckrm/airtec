b609d5756c3be6e91ff70ce15bdc67cf12923153
Katie Breuil <kcbreuil@gmail.com>
2021-06-14 23:33:24 -0400

    add bgimagewithcard components

--- web/src/pages/capabilities.js
+++ web/src/pages/capabilities.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Layout from '../components/layout';
 import Hero from '../components/hero';
 import ImageWithText from '../components/imageWithText';
+import BgImagewithCard from '../components/bgImagewithCard';
 
 export default function CapabilitiesPage({ data }) {
     const { heroImage, plane } = data;
@@ -21,11 +22,29 @@ export default function CapabilitiesPage({ data }) {
             text:
                 'Cras ut hendrerit lorem. Nunc tempus, est eu finibus porta, ante nisl molestie nunc, at gravida purus lectus a tellus. Aenean quis massa nulla. Mauris at leo quis diam finibus maximus. Donec cursus a metus id varius. Morbi feugiat, nisl vitae porttitor condimentum, tellus felis scelerisque leo, in cursus lacus augue nec justo.',
         },
+        info: {
+            heading: 'Services',
+            description:
+                'Etiam felis mauris, dapibus commodo ex a, vestibulum consequat neque. Vestibulum id auctor sapien. Vestibulum tempor tortor eget purus lobortis pretium.',
+        },
     };
     return (
         <Layout>
             <Hero data={content.hero} />
             <ImageWithText data={content.subHero} />
+            <BgImagewithCard
+                data={content.info}
+                image={plane}
+                hasButton
+                isSecondaryDark
+            />
+            <BgImagewithCard
+                data={content.info}
+                image={plane}
+                isRight
+                hasButton
+                isSecondaryDark
+            />
         </Layout>
     );
 }
