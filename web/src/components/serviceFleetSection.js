c551663ed5c71b1be79c607a717c9ae4ef5054cd
Katie Breuil <kcbreuil@gmail.com>
2021-06-16 14:09:35 -0400

    change to grid

--- web/src/components/serviceFleetSection.js
+++ web/src/components/serviceFleetSection.js
@@ -9,7 +9,7 @@ export default function ServiceFleetSection({ products, productTitle }) {
             <h2 className="font-bold text-4xl text-brand-1 text-center mb-12">
                 {productTitle}
             </h2>
-            <div className="flex flex-col items-center md:flex-row md:flex-wrap justify-center gap-9">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 place-items-center gap-9">
                 {products.map((item) => {
                     return <ServiceFleetCard data={item} key={item.id} />;
                 })}
