214bec80e8380c3926683cdf82aab06e4b04b908
Katie Breuil <kcbreuil@gmail.com>
2021-06-17 15:07:12 -0400

    change to section

--- web/src/components/certificationSection.js
+++ web/src/components/certificationSection.js
@@ -4,14 +4,14 @@ import Certification from './certification';
 
 export default function CertificationSection({ certifications }) {
     return (
-        <div className="bg-brand-1 py-14 text-white w-full">
+        <section className="bg-brand-1 py-14 text-white w-full">
             <div className="container grid grid-col gap-4">
                 <h2 className="text-ts-h2 uppercase">Certifications</h2>
                 {certifications.map((certification) => {
                     return <Certification data={certification} />;
                 })}
             </div>
-        </div>
+        </section>
     );
 }
 
