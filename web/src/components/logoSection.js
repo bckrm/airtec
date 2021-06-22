a72374fefe8a7b9773b346b43c58047c67d16723
Katie Breuil <kcbreuil@gmail.com>
2021-06-22 16:23:58 -0400

    ask for feedback

--- web/src/components/logoSection.js
+++ web/src/components/logoSection.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
 
-export default function LogoSection() {
-    return <section className="flex">{/* svgs will go here */}</section>;
+export default function LogoSection({ images }) {
+    return (
+        <section className="container grid grid-cols-2">
+            {images.map((image) => {
+                return <Img fluid={image} />;
+            })}
+        </section>
+    );
 }
+
+LogoSection.propTypes = {
+    images: PropTypes.array.isRequired,
+};
--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -5,21 +5,29 @@ import PropTypes from 'prop-types';
 import Layout from '../components/layout';
 import Hero from '../components/hero';
 import ContactSection from '../components/contactSection';
-// import LogoSection from '../components/logoSection';
+import LogoSection from '../components/logoSection';
 
 export default function Contact({ data }) {
-    const { heroImage } = data;
+    const { heroImage, kieyos, priority } = data;
     const content = {
         hero: {
             image: heroImage,
             text: 'Contact',
         },
     };
+    const images = [
+        {
+            image: kieyos,
+        },
+        {
+            image: priority,
+        },
+    ];
     return (
         <Layout>
             <Hero data={content.hero} />
             <ContactSection />
-            {/* <LogoSection /> */}
+            <LogoSection images={images} />
         </Layout>
     );
 }
@@ -33,11 +41,27 @@ export const query = graphql`
                 }
             }
         }
+        kieyos: file(relativePath: { regex: "/kieyos/" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        priority: file(relativePath: { regex: "/priority/" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
     }
 `;
 
 Contact.propTypes = {
     data: PropTypes.shape({
         heroImage: PropTypes.object.isRequired,
+        kieyos: PropTypes.object.isRequired,
+        priority: PropTypes.object.isRequired,
     }).isRequired,
 };
