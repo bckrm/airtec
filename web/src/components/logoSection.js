98395bd609bf8c4900eb4cfccd2b701240536468
Katie Breuil <kcbreuil@gmail.com>
2021-06-23 11:46:04 -0400

    update name of item

--- web/src/components/logoSection.js
+++ web/src/components/logoSection.js
@@ -5,8 +5,8 @@ import Img from 'gatsby-image';
 export default function LogoSection({ images }) {
     return (
         <section className="container gap-14 grid grid-cols-1 md:grid-cols-2 place-items-center mb-14">
-            {images.map((image) => {
-                return <Img fixed={image.image.childImageSharp.fixed} />;
+            {images.map((logo) => {
+                return <Img fixed={logo.image.childImageSharp.fixed} />;
             })}
         </section>
     );
