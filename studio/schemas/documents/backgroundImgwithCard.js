7b86ff7ac0bf9529746cf21f5ad595792038f743
Katie Breuil <kcbreuil@gmail.com>
2021-07-12 13:52:27 -0400

    add space

--- studio/schemas/documents/backgroundImgwithCard.js
+++ studio/schemas/documents/backgroundImgwithCard.js
@@ -15,7 +15,7 @@ export default {
         },
         {
             name: 'backgroundImage',
-            title: 'BackgroundImage',
+            title: 'Background Image',
             type: 'image',
         },
     ],
