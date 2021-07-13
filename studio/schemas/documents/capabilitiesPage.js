fb518af500b5d0ef1828b36a22132f110d4d889b
Katie Breuil <kcbreuil@gmail.com>
2021-07-13 11:30:11 -0400

    page addition

---
+++ studio/schemas/documents/capabilitiesPage.js
@@ -0,0 +1,24 @@
+export default {
+    name: 'capabilitiesPage',
+    title: 'Capabilities Page',
+    type: 'document',
+    __experimental_actions: [
+        'create',
+        'update',
+        // 'delete',
+        'publish',
+    ],
+    fields: [
+        {
+            name: 'pageTitle',
+            title: 'Page Title',
+            type: 'string',
+        },
+        {
+            name: 'heroImage',
+            title: 'Hero Image',
+            type: 'image',
+            validation: (Rule) => Rule.required(),
+        },
+    ],
+};
