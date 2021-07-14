29877cf575d41be2362a436b8b82fd4a6912d4ea
Katie Breuil <kcbreuil@gmail.com>
2021-07-14 13:52:02 -0400

    individual fleet page

---
+++ studio/schemas/documents/individualFleetItemPage.js.js
@@ -0,0 +1,29 @@
+export default {
+    name: 'individualFleetItemPage',
+    title: 'Individual Fleet Item Page',
+    type: 'document',
+    __experimental_actions: [
+        'create',
+        'update',
+        // 'delete',
+        'publish',
+    ],
+    fields: [
+        {
+            name: 'pageTitle',
+            title: 'Page Title',
+            type: 'string',
+        },
+        {
+            name: 'heroImage',
+            title: 'Hero Image',
+            type: 'image',
+            validation: (Rule) => Rule.required(),
+        },
+        {
+            name: 'fleetInfoBody',
+            title: 'Fleet Info Body',
+            type: 'bodyPortableText',
+        },
+    ],
+};
