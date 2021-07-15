68808270e30ed0d7b548de7c1d61d5ba8fe67e8c
Katie Breuil <kcbreuil@gmail.com>
2021-07-15 11:59:14 -0400

    outline

---
+++ studio/schemas/documents/service.js
@@ -0,0 +1,37 @@
+export default {
+    name: 'service',
+    title: 'Service',
+    type: 'document',
+    __experimental_actions: [
+        'create',
+        'update',
+        // 'delete',
+        'publish',
+    ],
+    fields: [
+        {
+            name: 'pageTitle',
+            title: 'Page Title',
+            type: 'string',
+        },
+        {
+            name: 'heroImage',
+            title: 'Hero Image',
+            type: 'image',
+            validation: (Rule) => Rule.required(),
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+            description:
+                'Brief description of this item. This text will appear on Fleet Card.',
+            validation: (Rule) => Rule.required().min(10).max(160),
+        },
+        {
+            name: 'serviceInfoBody',
+            title: 'Service Info Body',
+            type: 'bodyPortableText',
+        },
+    ],
+};
