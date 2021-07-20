9034f52914059943be05ee80798c4529ac385a2b
Katie Breuil <kcbreuil@gmail.com>
2021-07-20 13:44:49 -0400

    careers outline

---
+++ studio/schemas/documents/careerPage.js
@@ -0,0 +1,34 @@
+export default {
+    name: 'careerPage',
+    title: 'Career Page',
+    type: 'document',
+    __experimental_actions: [
+        'create',
+        'update',
+        // 'delete',
+        'publish',
+    ],
+    fields: [
+        {
+            name: 'pageTitle',
+            title: 'Page Title',
+            type: 'string',
+        },
+        {
+            name: 'heroImage',
+            title: 'Hero Image',
+            type: 'image',
+            validation: (Rule) => Rule.required(),
+        },
+        {
+            name: 'generalSellingPoints',
+            title: 'General Selling Points',
+            type: 'imageWithText',
+        },
+        {
+            name: 'jobsAtAirtec',
+            title: 'Jobs at Airtec',
+            type: 'imageWithText',
+        },
+    ],
+};
