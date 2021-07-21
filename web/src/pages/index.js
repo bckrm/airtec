1abbfd48afd39275e741f8e59c2161e517f53ce5
Katie Breuil <kcbreuil@gmail.com>
2021-07-21 15:43:10 -0400

    merge conflicts

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -7,42 +7,8 @@ import Hero from '../components/hero';
 import BgImagewithCard from '../components/bgImagewithCard';
 import NewsList from '../components/newsList';
 
-export const query = graphql`
-    query IndexQuery {
-        sanityIndexPage {
-            heroImage {
-                asset {
-                    gatsbyImageData(
-                        width: 2000
-                        placeholder: BLURRED
-                        formats: [AUTO, WEBP, AVIF]
-                    )
-                }
-            }
-            pageTitle
-        }
-
-        testImage: file(relativePath: { regex: "/hero/" }) {
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                }
-            }
-        }
-        plane: file(relativePath: { regex: "/plane/" }) {
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                }
-            }
-        }
-    }
-`;
-
 export default function IndexPage({ data }) {
     const {
-        heroImage,
-        plane,
         subHero,
         vision: { edges },
         whatWeDoSection,
@@ -100,19 +66,7 @@ export default function IndexPage({ data }) {
                 'Nam convallis arcu a quam tristique placerat. Maecenas egestas, nibh et ultricies tristique, purus magna sodales nisi, sit amet efficitur justo massa accumsan augue. Proin condimentum dolor elementum augue fermentum pretium. Phasellus consequat diam vel ante placerat fermentum sit amet vitae velit. Quisque vel ante ac erat accumsan tincidunt. am convallis arcu a quam tristique placerat. Maecenas egestas, nibh et ultricies tristique, purus magna sodales nisi, sit amet efficitur justo massa accumsan augue. Proin condimentum dolor elementum augue fermentum pretium. Phasellus consequat diam vel ante placerat fermentum sit amet vitae velit. Quisque vel ante ac erat accumsan tincidunt',
         },
     ];
-    const content = {
-        hero: {
-            image: heroImage,
-            text: 'Donec eleifend faucibus finibus maximus',
-        info: {
-            heading: 'this is a heading',
-            description:
-                'Etiam felis mauris, dapibus commodo ex a, vestibulum consequat neque. Vestibulum id auctor sapien. Vestibulum tempor tortor eget purus lobortis pretium.',
-        },
-        button: {
-            text: 'Learn More',
-        },
-    };
+
     return (
         <Layout>
             <Hero image={heroImage} title={pageTitle} />
@@ -142,20 +96,6 @@ export default function IndexPage({ data }) {
 
 export const query = graphql`
     query IndexQuery {
-        heroImage: file(relativePath: { regex: "/hero/" }) {
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                }
-            }
-        }
-        plane: file(relativePath: { regex: "/plane/" }) {
-            childImageSharp {
-                fluid {
-                    ...GatsbyImageSharpFluid
-                }
-            }
-        }
         subHero: allSanityIndexPage {
             edges {
                 node {
@@ -186,6 +126,33 @@ export const query = graphql`
                 }
             }
         }
+        sanityIndexPage {
+            heroImage {
+                asset {
+                    gatsbyImageData(
+                        width: 2000
+                        placeholder: BLURRED
+                        formats: [AUTO, WEBP, AVIF]
+                    )
+                }
+            }
+            pageTitle
+        }
+
+        testImage: file(relativePath: { regex: "/hero/" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        plane: file(relativePath: { regex: "/plane/" }) {
+            childImageSharp {
+                fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
     }
 `;
 
@@ -196,8 +163,6 @@ IndexPage.propTypes = {
         subHero: PropTypes.object.isRequired,
         vision: PropTypes.object.isRequired,
         whatWeDoSection: PropTypes.object.isRequired,
-        sanityIndexPage: PropTypes.object.isRequired,
-        testImage: PropTypes.object.isRequired,
         sanityIndexPage: PropTypes.object.isRequired,
         testImage: PropTypes.object.isRequired,
     }).isRequired,
