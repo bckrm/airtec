0080687437c3c28686cc16dfd58e297c6b230be7
Katie Breuil <kcbreuil@gmail.com>
2021-07-26 18:28:17 -0400

    update component

--- web/src/components/aboutTextSection.js
+++ web/src/components/aboutTextSection.js
@@ -6,12 +6,12 @@ import PropTypes from 'prop-types';
 import PlaneIconWithText from './svgs/PlaneIconWithText';
 
 export default function AboutText({ data }) {
-    const { textSectionOne, textSectionTwo } = data;
+    const { _rawLeftColumn, _rawRightColumn } = data;
 
     return (
         <div className="container gap-11 grid grid-cols-1 md:grid-cols-2 my-14">
             <p>
-                {textSectionOne}
+                {_rawLeftColumn}
                 <div className="mt-4">
                     <PlaneIconWithText data={data} />
                     <PlaneIconWithText data={data} />
@@ -19,7 +19,7 @@ export default function AboutText({ data }) {
                     <PlaneIconWithText data={data} />
                 </div>
             </p>
-            <p>{textSectionTwo}</p>
+            <p>{_rawRightColumn}</p>
         </div>
     );
 }
