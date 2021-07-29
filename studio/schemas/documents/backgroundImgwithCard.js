98e1829973f9921bab4517d990592e629a1cea3e
Katie Breuil <kcbreuil@gmail.com>
2021-07-29 13:15:49 -0400

    edits

--- studio/schemas/documents/backgroundImgwithCard.js
+++ studio/schemas/documents/backgroundImgwithCard.js
@@ -19,42 +19,26 @@ export default {
             type: 'image',
         },
         {
-            name: 'cardType',
-            title: 'Card Type',
+            name: 'cardBackgroundColor',
+            title: 'Card Background Color',
             type: 'string',
             description:
-                'Selection will give card and/or button a primary or secondary color scheme.',
+                'Selection will give the card a dark blue or transparent background color. The default color is white',
             options: {
                 list: [
-                    { title: 'Primary', value: 'primary' },
-                    { title: 'Secondary', value: 'secondary' },
-                ],
-                layout: 'radio',
-            },
-        },
-        {
-            name: 'backgroundColor',
-            title: 'Background Color',
-            type: 'string',
-            description:
-                'Selection will give card and/or button a white, blue or transparent background color.',
-            options: {
-                list: [
-                    { title: 'Light', value: 'light' },
                     { title: 'Dark', value: 'dark' },
                     { title: 'Transparent', value: 'transparent' },
                 ],
             },
         },
-
         {
             name: 'cardOrientation',
             title: 'Card Orientation',
             type: 'string',
-            description: 'Selection will align the card on the right.',
+            description:
+                'Selection will align the card on the right. Default is left oriented',
             options: {
                 list: [{ title: 'Right', value: 'right' }],
-                layout: 'radio',
             },
         },
         {
@@ -63,8 +47,22 @@ export default {
             type: 'string',
             description: 'Selection will give card a button',
             options: {
-                list: [{ title: 'hasButton', value: 'hasButton' }],
-                layout: 'radio',
+                list: [{ title: 'Button', value: 'button' }],
+            },
+        },
+        {
+            name: 'buttonType',
+            title: 'Button Type',
+            type: 'string',
+            description:
+                'Selection will give button a primary or secondary color scheme. If the card is light, choose a dark button',
+            options: {
+                list: [
+                    { title: 'Primary Light', value: 'primaryLight' },
+                    { title: 'Primary Dark', value: 'primaryDark' },
+                    { title: 'Secondary Light', value: 'secondaryLight' },
+                    { title: 'Secondary Dark', value: 'secondaryDark' },
+                ],
             },
         },
     ],
