a2a908b1d8f7d3a4adb5bdf40a825552646f79cf
Katie Breuil <kcbreuil@gmail.com>
2021-08-05 23:09:12 -0400

    404 pg

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 
-export default function PageNotFound() {
+import Layout from '../components/layout';
+import Hero from '../components/hero';
+
+export default function PageNotFound({ data }) {
+    const {
+        sanityIndexPage: { pageTitle, heroImage },
+    } = data;
+
     return (
-        <div>
-            <h2>Oops, looks like you are lost</h2>
-        </div>
+        <Layout>
+            <Hero image={heroImage} title={pageTitle} />
+            <div className="container flex flex-col justify-center max-w-lg my-40">
+                <h1 className="heading-accent pb-10 font-bold text-brand-1 text-ts-h3">
+                    Page Not Found
+                </h1>
+                <p>
+                    Looks like you&apos;ve followed a broken link or entered a
+                    URL that doesn&apos;t exist on this site.
+                </p>
+            </div>
+        </Layout>
     );
 }
+
+export const query = graphql`
+    query PageNotFoundQuery {
+        sanityIndexPage {
+            heroImage {
+                asset {
+                    gatsbyImageData(
+                        width: 2000
+                        placeholder: BLURRED
+                        formats: [AUTO, WEBP, AVIF]
+                    )
+                }
+            }
+            pageTitle
+        }
+    }
+`;
+
+PageNotFound.propTypes = {
+    data: PropTypes.shape({
+        sanityIndexPage: PropTypes.object.isRequired,
+    }).isRequired,
+};
