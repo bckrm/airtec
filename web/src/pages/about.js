aee6fd183ec2c548634cce3431cfee9f6973c814
Katie Breuil <kcbreuil@gmail.com>
2021-08-09 17:27:51 -0400

    images rendering, but need to debug framer/load issue

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -10,6 +10,27 @@ import AboutText from '../components/aboutTextSection';
 import HistorySection from '../components/historySection';
 
 export const query = graphql`
+    fragment SanityImage on SanityMainImage {
+        crop {
+            _key
+            _type
+            top
+            bottom
+            left
+            right
+        }
+        hotspot {
+            _key
+            _type
+            x
+            y
+            height
+            width
+        }
+        asset {
+            _id
+        }
+    }
     query AboutQuery {
         sanityAboutPage {
             heroImage {
@@ -52,9 +73,7 @@ export const query = graphql`
                     historyText
                     id
                     image {
-                        asset {
-                            gatsbyImageData
-                        }
+                        ...SanityImage
                     }
                 }
             }
