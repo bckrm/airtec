9e26e65e57f292f4edface312f0247cc533e6a10
Katie Breuil <kcbreuil@gmail.com>
2021-08-16 11:23:59 -0400

    button working

--- web/src/components/imageWithText.js
+++ web/src/components/imageWithText.js
@@ -3,25 +3,14 @@ import PropTypes from 'prop-types';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 import BlockText from './block-content/blockText';
-import Button from './button';
 
-export default function ImageWithText({
-    data,
-    isLeft,
-    hasButton,
-    isPrimaryDark,
-    isPrimary,
-    isSecondaryDark,
-    isSecondary,
-    isTransparent,
-    to,
-    href,
-    anchorId,
-}) {
+export default function ImageWithText({ data, isLeft, anchorId }) {
     const {
         heading,
         _rawInfo,
         image: { asset },
+        buttonText,
+        buttonURL,
     } = data;
 
     const imageData = getImage(asset);
@@ -42,16 +31,14 @@ export default function ImageWithText({
                     <BlockText blocks={_rawInfo} />
                 </p>
                 <div className="w-auto pt-4">
-                    {hasButton && (
-                        <Button
-                            isPrimaryDark={isPrimaryDark}
-                            isSecondary={isSecondary}
-                            isPrimary={isPrimary}
-                            isSecondaryDark={isSecondaryDark}
-                            isTransparent={isTransparent}
-                            href={href}
-                            to={to}
-                        />
+                    {buttonURL && (
+                        <a
+                            href={buttonURL}
+                            type="button"
+                            className="font-semibold border-2 inline-block leading-tight px-8 py-2 mb-6 rounded-bl-2xl tracking-[.12rem] transition-colors uppercase bg-brand-1 border-brand-1 text-white hover:bg-brand-2 hover:border-brand-2 focus:bg-brand-2"
+                        >
+                            {buttonText}
+                        </a>
                     )}
                 </div>
             </div>
@@ -60,28 +47,12 @@ export default function ImageWithText({
 }
 
 ImageWithText.defaultProps = {
-    to: '',
-    href: '',
     isLeft: false,
-    isPrimary: false,
-    isPrimaryDark: false,
-    isSecondary: false,
-    isSecondaryDark: false,
-    hasButton: false,
-    isTransparent: false,
     anchorId: '',
 };
 
 ImageWithText.propTypes = {
     data: PropTypes.object.isRequired,
     isLeft: PropTypes.bool,
-    isPrimary: PropTypes.bool,
-    isPrimaryDark: PropTypes.bool,
-    isSecondary: PropTypes.bool,
-    isSecondaryDark: PropTypes.bool,
-    hasButton: PropTypes.bool,
-    isTransparent: PropTypes.bool,
-    to: PropTypes.string,
-    href: PropTypes.string,
     anchorId: PropTypes.string,
 };
